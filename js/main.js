cfa80ed8ad54b8faa1f6dd3a308940d651941975
Duvan Molina <duvan074@gmail.com>
2019-08-01 13:58:35 -0500

    todo listo

--- js/main.js
+++ js/main.js
@@ -3,14 +3,19 @@ function Calcular() {
   var peso = document.getElementById('peso').value;
   var select = document.getElementById('planetas').value;
   var gravedad = 9.81;
-  var velocidadT = 1.622;
-  var velocidadM = 3.71;
-  var velocidadV = 8.87;
+  var fT = 1.622;
+  var fM = 3.71;
+  var fV = 8.87;
+
 
   function validar() {
     if (peso == ''){
-      alert('por favor ingresa correctamente el peso')
+      alert('por favor ingresa correctamente el peso');
+      return false;
     } 
+    else if(select == 'nada'){
+      alert('por favor elige un planeta');
+    }
     else  { 
       datos();
     }
@@ -19,17 +24,17 @@ function Calcular() {
 
   function datos() {
     if(select == 'planeta1') {
-      var resultado = peso/gravedad*velocidadT;
+      var resultado = peso/gravedad*fT;
       document.getElementById('span').innerHTML = 'Tu peso es: ' +  Math.round(resultado*100)/100  + ' kg';
     } 
     
     else if (select == 'planeta2') {
-      var resultadoM = peso/gravedad*velocidadM;
+      var resultadoM = peso/gravedad*fM;
       document.getElementById('span').innerHTML = 'Tu peso es: ' + Math.round(resultadoM*100)/100 + ' kg';
     }
     
     else {
-      var resultadoV = peso/gravedad*velocidadV;
+      var resultadoV = peso/gravedad*fV;
       document.getElementById('span').innerHTML = 'Tu peso es: ' + Math.round(resultadoV*100)/100 + ' kg';
     }
   }
